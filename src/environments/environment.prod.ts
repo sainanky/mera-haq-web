4504f811d138b949e90cf17e2d73b00435e82774
Rohit Singh <rohitsingh@Rohits-MacBook-Pro.local>
2023-02-17 23:04:09 +0530

    all

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  url : 'https://api.finanvo.in',
+  apiKey : '8i1gNe4cIU',
+  apiSecretKey : 'ponaryoVPXAwZQkaLZTF4scER57BbJAkftyQCpM3',
+  defaultImage : 'https://via.placeholder.com/100x100.png',
+  payment_api_url : 'https://payment.finanvo.in'
+};
